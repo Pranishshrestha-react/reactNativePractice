dea105a7ab1bf1fb3866fd5960fff54e15314922
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-11 15:58:56 +0545

    home created

--- App.js
+++ App.js
@@ -1,70 +1,27 @@
-
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.main}>
-      <View style={styles.first}>
-        <Text style={styles.text}>Conversation</Text>
-        
-      </View>
-      <View style={styles.second}>
-          <Text style={styles.text}>Conversation 1</Text>
-          <Text style={styles.text}>Conversation 2</Text>
-          <Text style={styles.text}>Conversation 3</Text>
-          <Text style={styles.text}>Conversation 4</Text>
-        
-      </View>
-      <View style={styles.third}>
-        <View style={styles.thirdfirst}>
-          <Text style={styles.text}>A</Text>
-          <Text style={styles.text}>B</Text>
-        </View>
-        <View style={styles.thirdsecond}>
-          <Text style={styles.text}>C</Text>
-          <Text style={styles.text}>D</Text>
-        </View>
-      </View>
+export default App() {
+  return(
+    <View style={styles.home}>
+    <Text style={styles.text}>
+    Hello nepal</Text>
+    
+    
     </View>
+
   );
 
 }
 
-const styles = StyleSheet.create({
-  main:{
+const styles= StyleSheet.create({
+  home:{
     flex: 1,
-    marginTop: 20
-  },
-  first:{
-   flex:1,
-   justifyContent:'center',
-   alignItems:'center',
-    backgroundColor: '#f00',
+    backgroundColor: '#f11',
   },
   text:{
-    fontSize: 20,
-    color: 'white',
-    
-  },
-  second:{
-    flex: 8,
-    justifyContent:'flex-end',
-    alignItems:'flex-end',
-    backgroundColor: '#0f0',
-  },
-  third:{
-    flex: 4,
-    justifyContent:'space-between',
-    backgroundColor: '#00f',
-  },
-  thirdfirst:{
-    flexDirection: 'row',
-    justifyContent:'space-between'
-  },
-  thirdsecond:{
-    flexDirection:'row',
-    justifyContent:'space-between'
-
+    fontSize: 30,
+    color: '#fff',
   }
-});
+
+})
