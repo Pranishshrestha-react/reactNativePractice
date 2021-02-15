998df162252e511e760c3135190f1faaaea01183
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-15 17:03:22 +0545

    shorten

--- App.js
+++ App.js
@@ -29,10 +29,7 @@ export default function App() {
           <View style={styles.menu}>
             <Card price="$ 23.00" name ={{uri:"https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/03/033_FR11785.jpg?fit=978%2C650&ssl=1"}} itemname="American Burger" itemdes="simply dummy text of the printing and typesetting industry. Lorem" time="45 min" rating="4.5"/>
             <Card price="$ 20.00" name ={{uri:"https://www.skinnytaste.com/wp-content/uploads/2015/05/grilled-teriyaki-burger-550x825.jpg"}} itemname="Cheese Hot Dog" itemdes="simply dummy text of the printing and typesetting industry. Lorem" time="60 min" rating="4"/>
-            <Card price="$ 22.00" name ={{uri:"https://cdn.vox-cdn.com/thumbor/73nhsZwI55BVgH8-rapmIhkvbUk=/0x0:4047x3035/1200x675/filters:focal(1700x1194:2346x1840)/cdn.vox-cdn.com/uploads/chorus_image/image/64046617/20150915-_Upland_Burger_3.0.0.1489236245.0.jpg"}} itemname="Chicken Burger" itemdes="simply dummy text of the printing and typesetting industry. Lorem" time="20 min" rating="3"/>
-            <Card price="$ 21.50" name ={{uri:"https://twisper.com/wp-content/uploads/2020/03/close-up-photo-of-burger-3915906-scaled.jpg"}} itemname="Ham Burger" itemdes="simply dummy text of the printing and typesetting industry. Lorem" time="50 min" rating="3.5"/>
-            <Card price="$ 25.00" name ={{uri:"https://happilyunprocessed.com/wp-content/uploads/2018/03/Juicest-Burger-Everfeature.jpg.jpg"}} itemname="Pork Burger" itemdes="simply dummy text of the printing and typesetting industry. Lorem" time="52 min" rating="5"/>
-                     
+               
             
           </View>
 
