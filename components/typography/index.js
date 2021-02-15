351c0456d49326c80fab747be496827142f38d3e
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-15 16:55:21 +0545

    typography made

---
+++ components/typography/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const Title =(props) => {
+    return <Text style={styles.head}> {props.children}</Text>
+}
+const SubTitle =(props) => {
+    return <Text style={styles.subhead}> {props.children}</Text>
+}
+
+const AllText =(props) => {
+    return <Text style={styles.alltext}> {props.children} </Text>
+}
+
+const styles= StyleSheet.create({
+    head:{
+        fontSize: 18,
+        fontWeight: "bold",
+
+    },
+    subhead:{
+        fontSize: 15,
+        fontWeight: "bold",
+    },  
+    alltext:{
+        fontSize: 10,
+        color: '#B6BEB7',
+    }
+});
+
+export {Title , AllText, SubTitle};
