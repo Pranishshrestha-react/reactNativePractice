f84c554e19251403f519d4a5c389809a4621dae9
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-22 17:01:00 +0545

    FILTER CREATED

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { View, StyleSheet, Text, ScrollView,StatusBar,Modal} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import { View, StyleSheet, Text, ScrollView,Modal} from 'react-native';
 import Header from './components/header';
 import Itemproducts from './components/itemproducts';
 import {Ionicons} from '@expo/vector-icons';
 import Additem from './components/additems';
+import Filter from './components/filter';
 
  
 export default function App() {
   const [isOpen, setIsOpen] = React.useState(false)
-
+  const [isFilter, setIsFilter] = React.useState(false)
+  const [imgblocks, addImgblock] = React.useState([])
   
   const handleSubmit=(pdata)=>{
+    addImgblock([{...pdata, id: Math.random().toString()}, ...imgblocks ])  
     setIsOpen(false)
   }
   return(
    
   <View style={styles.home}>
-    <StatusBar hidden />
-    <Header />
+    <StatusBar hidden/>
+    <Header filterpage={()=> setIsFilter(true)}/>
     <ScrollView>
-    <Itemproducts />
+    <Itemproducts imgblocks={imgblocks} />
     </ScrollView>
     <View style={styles.add}>
       <Ionicons name='add-outline' size={50} color='#047C18' onPress={() => setIsOpen(true)}/>
@@ -27,6 +31,9 @@ export default function App() {
     <Modal visible ={isOpen}>
       <Additem cancel={() => setIsOpen(false)} submit={handleSubmit} />
 
+    </Modal>
+    <Modal visible ={isFilter}>
+      <Filter cancel={() => setIsFilter(false)}/>
     </Modal>
   </View>
  
