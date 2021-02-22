3342a7869638b97816bc38a9b50a2d38fe70fabc
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-22 15:36:56 +0545

    category file made

---
+++ constant/categories/index.js
@@ -0,0 +1,24 @@
+const CATEGORIES = [
+    {
+        id: 1,
+        title: 'Automobiles'
+    },
+    {
+        id: 2,
+        title: 'Electronics'
+    },
+    {
+        id: 3,
+        title: 'Sports Equipment'
+    },
+    {
+        id: 4,
+        title: 'Clothes'
+    },
+    {
+        id: 5,
+        title: 'Shoes'
+    },
+]
+
+export default CATEGORIES;
